6922c730db170c3ba91d8356798baf92c2ca9af1
Jane Smith <alison.mcirvin@gmail.com>
2017-09-28 20:59:19 -0700

    edited css and html

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# about-me
+This is an about me page thaty included a fun guessing game to help the user get to know me a bit and also to practice my newly learned javascript skills.
---
+++ app.js
@@ -0,0 +1,46 @@
+'use strict';
+// var user = prompt('Well Hello, please introduce yourself');
+// console.log('The user\'s name is ' + user);
+//
+// alert('Hi there, ' + user + ', I have some questions for you today.');
+//
+// var answer1 = prompt(user + ', this is a yes/no quesion so please answer with a Y or N. Do I like to hike?');
+// console.log('The user answered Question 1:' + answer1);
+//
+// if(answer1 === 'Y' || answer1 === 'y') {
+//   alert('Good guess! Alison does like to hike');
+//
+// } else if(answer1 === 'N' || answer1 === 'n')
+//   alert('of course she likes to hike,');
+//
+// alert('Let\'s keep going, but answer Y or N, ok? There are rules to follow.');
+//
+// var question2 = prompt('So, ' + user + ' Do I have children? Please answer with a Y or N');
+//
+// console.log('The user answered Question 2:' + question2);
+//
+// if(question2 === 'Y' || question2 === 'y') {
+//   alert('Good guess! Alison does have children');
+//
+// } else if(question2 === 'N' || question2 === 'n') {
+//
+//   alert('She sure does, 5 in fact and they all have flown the nest!');
+// }
+//
+// var question3 = prompt('Next question, Am I a Husky? (Y/N again, thanks!)');
+//
+// console.log('The user answered Question 3:' + question3);
+//
+// if(question3 === 'Y' || question3 === 'y') {
+//
+//   alert('Nope! Alison is a Duck and proud of that, QUACK!');
+// } else if(question2 === 'N' || question2 === 'n') {
+//   alert('That is right, Alison is in fact a Duck, no way a Husky!');
+// }
+//
+// var question4 = prompt('Almost done, what is my dream profession?');
+//
+// console.log('The user answered Question 4:' + question4);
+// {
+//   alert('Front End Web Developer is Alison\'s dream. Thanks ' + user + ' for playing along');
+// }
