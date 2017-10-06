e90e09788ba406e7b725c3c2e684c5f34f532a15
Jane Smith <alison.mcirvin@gmail.com>
2017-10-05 18:22:40 -0700

    clean up with Mike Thursday

--- app.js
+++ app.js
@@ -18,10 +18,10 @@ if(answer1 === 'y') {
 alert('Let\'s keep going, but answer Y or N, ok? There are rules to follow.');
 
 for (var i = 0; i <= 7; i++) {
- + console.log(answer1[i]);
- userPoints++;
- alert('you have ' + userPoints + ' points.');
-
+  console.log(answer1[i]);
+  userPoints++;
+  alert('you have ' + userPoints + ' points.');
+}
 var question2 = prompt('So, ' + user + ' Do I have children? Please answer with a Y or N').toLowerCase();
 
 console.log('The user answered Question 2:' + question2);
@@ -35,13 +35,13 @@ if(question2 === 'y') {
   userPoints++;
 } else {
 }
-  //if it's not correct, tell them to try again
-  alert('Nope, try again.');
-  for (var i = 0; i <= 6; i++) {
-   + console.log(answer1[i]);
-   userPoints++;
-   alert('you have ' + userPoints + ' points.');
-
+//if it's not correct, tell them to try again
+alert('Nope, try again.');
+for (var i = 0; i <= 6; i++) {
+  console.log(answer1[i]);
+  userPoints++;
+  alert('you have ' + userPoints + ' points.');
+}
 var question3 = prompt('Next question, Am I a Husky?').toLowerCase();
 
 console.log('The user answered Question 3:' + question3);
@@ -53,20 +53,20 @@ if(question3 === 'y') {
   alert('That is right, Alison is in fact a Duck, no way a Husky!');
 }
 for (var i = 0; i <= 5; i++) {
- + console.log(answer1[i]);
- userPoints++;
- alert('you have ' + userPoints + ' points.');
- }
+  console.log(answer1[i]);
+  userPoints++;
+  alert('you have ' + userPoints + ' points.');
+}
 var question4 = prompt('Next question, what is my go-to free time activity?');
 
 console.log('The user answered Question 4:' + question4);
 alert('Hiking is the only way to go! ');
 
 for (var i = 0; i <= 4; i++) {
- + console.log(answer1[i]);
- userPoints++;
- alert('you have ' + userPoints + ' points.');
- }
+  console.log(answer1[i]);
+  userPoints++;
+  alert('you have ' + userPoints + ' points.');
+}
 
 var question5 = prompt('Almost done, what is my dream profession?');
 
@@ -74,37 +74,37 @@ console.log('The user answered Question 5:' + question5);
 alert('Front End Web Developer is Alison\'s dream job.');
 
 for (var i = 0; i <= 3; i++) {
- + console.log(answer1[i]);
- userPoints++;
- alert('you have ' + userPoints + ' points.');
- }
+  console.log(answer1[i]);
+  userPoints++;
+  alert('you have ' + userPoints + ' points.');
+}
 
- var question6 = prompt('Almost done, how many children does Alison have? You have 6 attempts to get this right!');
-  if userAnswer === 5 {
-    document.write('You know her well! Alison has 5 children.')
-  } else {
-    document.write('Try again, you have 5 more attempts');
-  }
-
- console.log('The user answered Question 6:' + question6);
- alert('You know her well! Alison has 5 children.');
-var question6 = ['5', 'five'];
-var promptExit = true;
+var question6 = prompt('Almost done, how many children does Alison have? You have 6 attempts to get this right!');
+if (question6 === 5) {
+  document.write('You know her well! Alison has 5 children.');
+} else {
+  document.write('Try again, you have 5 more attempts');
+}
 
 for (var i = 0; i < 8 && promptExit; i++) {
   var answer = prompt('Can you name some of Alison\'s favorite countries she has visited?').toLowerCase();
+
   console.log(answer);
+
   if (answer === countriesVisited[0] || answer === countriesVisited[1] || answer === countriesVisited[2] || answer === countriesVisited[3] || answer === countriesVisited[4] || answer === countriesVisited[5]) {
+
     alert('Great, ' + answer + 'is correct');
     promptExit = false;
   }else if (i === 7){
+
     alert('You are out of attempts');
   }
   else{
     alert('Keep trying');
   }
   for (var i = 0; i <= 2; i++) {
-   + console.log(answer1[i]);
-   userPoints++;
-   alert('you have ' + userPoints + ' points.');
-   }
+    console.log(answer1[i]);
+    userPoints++;
+    alert('you have ' + userPoints + ' points.');
+  }
+}
